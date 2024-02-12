f5b35213a9737ee95f07d0cc676147135f00ede9
Dandan <dandant3@gmail.com>
2024-02-12 10:52:42 +0200

    first 2 problems not finished

--- ArrayOps.java
+++ ArrayOps.java
@@ -1,17 +1,38 @@
+import java.util.List;
+
 public class ArrayOps {
     public static void main(String[] args) {
-        
-    }
-    
+    } 
     public static int findMissingInt (int [] array) {
-        // Write your code here:
-        return -1;
-    }
-
+        int n = array.length + 1;
+        int sumtotal = 0;
+        int MissingNumber = 0;
+        for (int i = 0; i < n; i++) {
+            sumtotal  += i;
+            }
+        MissingNumber = sumtotal - sumArry(array);
+        return MissingNumber;
+        }
+                
     public static int secondMaxValue(int [] array) {
-        // Write your code here:
-        return 0;
-    }
+        int maxIndex = 0;
+        int maxValue = array[0]; // Initialize max with the first element of the array
+        for (int i = 1; i < array.length; i++) {
+            if (array[i] > maxValue) {
+                maxValue = array[i]; // Update maxValue if current element is greater
+                maxIndex = i;
+            }
+        }
+        int maxsecond = array[0];
+        for (int j = 0; j < array.length; j++) {
+            if( array[j] > maxsecond && j != maxIndex)
+                maxsecond = array[j];
+            }
+            return maxsecond;
+        }
+        
+
+    
 
     public static boolean containsTheSameElements(int [] array1,int [] array2) {
         // Write your code here:
@@ -22,5 +43,24 @@ public class ArrayOps {
         // Write your code here:
         return false;
     }
+    public static int sumArry(int[] array){
+        int sum = 0 ;
+        for (int i = 0; i < array.length; i++) {
+        sum += array[i];
+        }
+        return sum ;
+    }
 
+    public static int maxvalue(int[] array){
+        int maxIndex = 0;
+        int maxValue = array[0]; // Initialize max with the first element of the array
+        for (int i = 1; i < array.length; i++) {
+            if (array[i] > maxValue) {
+                maxValue = array[i]; // Update maxValue if current element is greater
+                maxIndex = i;
+            }
+        }
+        return maxValue;
+    }
+    
 }
